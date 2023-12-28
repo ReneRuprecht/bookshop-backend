65ab2001849857238b0e995369a40287c238a807
ReneRuprecht <rene.ruprecht.396@gmail.com>
2023-12-28 21:14:04 +0100

    Update: Tests for inventory service

--- src/test/java/com/example/bookshop/inventory/InventoryServiceImplTest.java
+++ src/test/java/com/example/bookshop/inventory/InventoryServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.example.bookshop.inventory;
 
 import com.example.bookshop.inventory.util.MapperImpl;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,6 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -81,18 +81,89 @@ class InventoryServiceImplTest {
         verify(mapperImpl, times(2)).convertBookToBookDto(any());
     }
 
-    @Disabled
     @Test
     void shouldFindABookByIsbn() {
+        String isbnToSearch = "1";
+        Book book1 = Book.builder()
+                         .isbn(isbnToSearch)
+                         .title("t1")
+                         .description("d1")
+                         .price(BigDecimal.valueOf(1))
+                         .build();
+
+        BookDto bookDto1 = BookDto.builder()
+                                  .isbn(isbnToSearch)
+                                  .title("t1")
+                                  .description("d1")
+                                  .price(BigDecimal.valueOf(1))
+                                  .build();
+
+        when(mapperImpl.convertBookToBookDto(book1)).thenReturn(bookDto1);
+        when(inventoryRepository.findByIsbn(isbnToSearch)).thenReturn(Optional.of(book1));
+
+        BookDto actual = underTest.findByIsbn(isbnToSearch);
+
+        assertNotNull(actual);
+
+        verify(mapperImpl, times(1)).convertBookToBookDto(book1);
+        verify(inventoryRepository, times(1)).findByIsbn(isbnToSearch);
+
+        assertEquals(bookDto1, actual);
     }
 
-    @Disabled
     @Test
     void shouldDeleteABookByIsbn() {
+        String isbnToDelete = "1";
+
+        Book book1 = Book.builder()
+                         .isbn(isbnToDelete)
+                         .title("t1")
+                         .description("d1")
+                         .price(BigDecimal.valueOf(1))
+                         .build();
+
+        when(inventoryRepository.findByIsbn(isbnToDelete)).thenReturn(Optional.ofNullable(book1));
+        assertNotNull(book1);
+
+        String actual = underTest.deleteByIsbn(isbnToDelete);
+
+        verify(inventoryRepository, times(1)).findByIsbn(isbnToDelete);
+        verify(inventoryRepository, times(1)).delete(book1);
+
+        assertEquals(isbnToDelete, actual);
     }
 
-    @Disabled
     @Test
     void shouldUpdateABook() {
+        String isbn = "1";
+        Book book1 = Book.builder()
+                         .isbn(isbn)
+                         .title("t1")
+                         .description("d1")
+                         .price(BigDecimal.valueOf(1))
+                         .build();
+
+        BookDto bookDto1 = BookDto.builder()
+                                  .isbn(isbn)
+                                  .title("t2")
+                                  .description("d2")
+                                  .price(BigDecimal.valueOf(2))
+                                  .build();
+
+        Book expected = Book.builder()
+                            .isbn(isbn)
+                            .title("t2")
+                            .description("d2")
+                            .price(BigDecimal.valueOf(2))
+                            .build();
+
+        when(inventoryRepository.findByIsbn(isbn)).thenReturn(Optional.ofNullable(book1));
+
+        String actual = underTest.update(bookDto1);
+
+        verify(inventoryRepository, times(1)).save(expected);
+        verify(inventoryRepository, times(1)).findByIsbn(isbn);
+
+        assertEquals(actual, expected.getIsbn());
     }
 }
